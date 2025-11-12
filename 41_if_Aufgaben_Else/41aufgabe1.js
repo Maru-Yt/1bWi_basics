7bd4642b0382c3423ec05750fe3d0e8d2d91c99d
Your Name <you@example.com>
2025-11-12 16:25:35 +0100

    41aufgabe1.js

---
+++ 41_if_Aufgaben_Else/41aufgabe1.js
@@ -0,0 +1,20 @@
+let number = 7;
+let number2 = 3;
+let number3 = 0;
+
+if (number > 0) {
+    console.log("The number is positive");
+} 
+else {
+    console.log("The number is not positive");
+}
+if (number2 < 0) {
+    console.log("The number is negative");
+} else {
+    console.log("The number is positive or 0");
+}
+if (number3 == 0) {
+    console.log("The number is 0"); 
+} else {
+    console.log("The number is not 0");
+}
