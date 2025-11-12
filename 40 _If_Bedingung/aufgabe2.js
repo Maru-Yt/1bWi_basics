678668defdb0557f04affa14134eae64f42f38ad
Your Name <you@example.com>
2025-11-12 16:04:20 +0100

    aufgabe2.js

---
+++ 40 _If_Bedingung/aufgabe2.js
@@ -0,0 +1,10 @@
+let trafficlight = "yellow";
+if (trafficlight === "red") {
+    console.log("stop!");
+}
+if (trafficlight === "yellow") {
+    console.log("caution!");
+}
+if (trafficlight === "green") {
+    console.log("You may go");
+}
