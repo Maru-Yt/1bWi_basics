6b915f07e2d58b8236f06e92a8e70d08b913dba3
Your Name <you@example.com>
2025-11-12 15:55:16 +0100

    aufgabe1.js

---
+++ 40 _If_Bedingung/aufgabe1.js
@@ -0,0 +1,10 @@
+let number = 7;
+if (number > 0) {
+  console.log('the number is positive');
+}
+if (number < 0) {
+    console.log('the number is negative');
+}
+if (number == 0) {
+    console.log('the number is zero');
+}
