6ea7840e66292d73b89674f44ad6c6153e72d178
Your Name <you@example.com>
2025-11-17 11:28:14 +0100

    42_1Aufgabe.If_Bedingungen.js

---
+++ 42_if_Bedingungen_else/42_1Aufgabe.If_Bedingungen.js
@@ -0,0 +1,9 @@
+let randomNumber = Math.floor(Math.random() * 10); 
+if (randomNumber < 5); 
+print("The number is less than 5");
+if (randomNumber >= 5);
+print("The number is greater than 5");
+if (randomNumber == 5);
+print("The number is exactly  5");
+
+ 
