959d754ebdb4f5e3ba41a5f8c4d15fff818c7ab2
Your Name <you@example.com>
2025-12-03 16:10:49 +0100

    4.Aufgabe.deno.js

--- 45_Deno/3.Aufgabe.deno.js
+++ 45_Deno/3.Aufgabe.deno.js
@@ -1,9 +1,12 @@
 let number1 = 15;
 let number2 = 7;
-answer = number1 + number2;
+let answer = number1 + number2;
 if (answer < 0) 
 console.log("The answer is negative and false");
+else if (answer == 0) {
+console.log("The answer is zero");
+} else if (answer == 22 && answer >= 0) 
+console.log("The answer is 22 and positive");
 else if (answer > 0) {
-console.log("The answer is positive but not true");
-} else if (answer == 22) 
-console.log("The answer is 22 and true");
+console.log("The answer is positive and btu not true");
+}
