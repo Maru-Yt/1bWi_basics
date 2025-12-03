16d887598bd38779f6f93d8278e0fe3548e9916b
Your Name <you@example.com>
2025-12-03 15:50:20 +0100

    3.Aufgabe.deno.js

---
+++ 45_Deno/3.Aufgabe.deno.js
@@ -0,0 +1,9 @@
+let number1 = 15;
+let number2 = 7;
+answer = number1 + number2;
+if (answer < 0) 
+console.log("The answer is negative and false");
+else if (answer > 0) {
+console.log("The answer is positive but not true");
+} else if (answer == 22) 
+console.log("The answer is 22 and true");
