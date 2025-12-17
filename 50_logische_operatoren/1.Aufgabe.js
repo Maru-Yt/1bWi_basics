e94311df38c6eee52987cdd395a73b02fe105396
Your Name <you@example.com>
2025-12-17 16:19:15 +0100

    1.Aufgabe.js

---
+++ 50_logische_operatoren/1.Aufgabe.js
@@ -0,0 +1,13 @@
+let a = 99.67;
+
+if (a >= 90 && a <= 100) {
+    console.log("Grade: A");
+} else if (a >= 80 && a < 90) {
+    console.log("Grade: B");
+} else if (a >= 70 && a < 80) {
+    console.log("Grade: C");
+} else if (a >= 60 && a < 70) {
+    console.log("Grade: D");
+} else if (a >= 0 && a < 60) {
+    console.log("Grade: F");
+}   console.log("");
