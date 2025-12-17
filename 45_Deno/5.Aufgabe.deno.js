ddd5ada7f26cf36d2e7fe12fff584dd612880b79
Your Name <you@example.com>
2025-12-17 15:37:40 +0100

    5.Aufgabe.deno.js

---
+++ 45_Deno/5.Aufgabe.deno.js
@@ -0,0 +1,17 @@
+let question= prompt("was ist die Temperatur heute?");
+let temp= Number (question);
+
+if (temp <= 0) {
+    console.log("Drinnen bleiben");
+} else if (temp >0 && temp <=20) {
+    console.log("Warm anziehen ");
+} else if (temp >20 && temp <=35) {
+    console.log("baden gehen");
+} else if (temp >35) {
+    console.log("In die Kühlbox gehen");
+} else if (temp >20 && temp <=35) {
+    console.log("baden gehen");
+} else if (temp >35) {
+    console.log("In die Kühlbox gehen");
+}
+console.log("Fertig");
