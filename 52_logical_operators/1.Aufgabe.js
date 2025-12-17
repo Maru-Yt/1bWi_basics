cf63f857a9c61eba3236abdfb226c45dee1db0ef
Your Name <you@example.com>
2025-12-17 16:31:30 +0100

    1.Aufgabe.js

---
+++ 52_logical_operators/1.Aufgabe.js
@@ -0,0 +1,8 @@
+let temp = 25;
+let humidity = 55;
+
+if (temp >= 20 && temp <= 25 && humidity < 60) {
+    console.log("Perfect day for outdoor activities!");
+} else {
+    console.log("Maybe stay indoors today.");
+}
