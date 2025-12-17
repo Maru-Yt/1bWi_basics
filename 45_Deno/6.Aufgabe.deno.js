b9879ad7f2c94960b3835409eb0fa40d6ace16ed
Your Name <you@example.com>
2025-12-17 15:55:20 +0100

    6.Aufgabe.deno.js

---
+++ 45_Deno/6.Aufgabe.deno.js
@@ -0,0 +1,15 @@
+let question= prompt("Was für Getränk möchten Sie nehmen? 1: Tee, 2: Cola, 3: Wasser, 4: Bier");
+let temp= Number (question);
+
+if (temp == 1) {
+    console.log("Danke für deine Bestellung von Tee");
+}
+else if (temp == 2) {
+    console.log("Danke für deine Bestellung von Cola");
+}
+else if (temp == 3) {
+    console.log("Danke für deine Bestellung von Wasser");
+}
+else if (temp == 4) {
+    console.log("Danke für deine Bestellung von Bier");
+}
