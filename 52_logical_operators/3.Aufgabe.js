9fe11f253a16e0c136eeed8a4155bf2392f114b8
Your Name <you@example.com>
2026-01-06 18:50:53 +0100

    3.Aufgabe.js

---
+++ 52_logical_operators/3.Aufgabe.js
@@ -0,0 +1,9 @@
+let grade = 65;
+let attendance = 80;
+let extraCredit = 3;
+
+if ((grade >= 80 && attendance >= 90) || extraCredit) {
+    console.log("Eligible for advanced exam");
+} else  {
+    console.log("Not eligible for advanced exam.");
+}
