69baa9e48187b384f1cb75de228869f4218fb255
Your Name <you@example.com>
2026-01-06 19:09:16 +0100

    5.Aufgabe.js

---
+++ 52_logical_operators/5.Aufgabe.js
@@ -0,0 +1,7 @@
+let age = 40;
+let heartRate = 155; // beats per minute
+if (age <= 30 && heartRate < 100)   {
+    console.log("Light exercise.");
+} else {
+    console.log("Go to the doctor.");
+}
