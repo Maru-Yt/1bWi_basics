3551dedb16b01a4c6493491313f30b6b1770efe0
Your Name <you@example.com>
2026-01-06 19:00:38 +0100

    4.Aufgabe.js

---
+++ 52_logical_operators/4.Aufgabe.js
@@ -0,0 +1,9 @@
+let age = 20;
+let money = 15;
+if (age >= 18 && money >= 12) {
+    console.log("Ticket purchased!");
+} else if (age >= 18 && money < 12) {
+    console.log("Not enough money.");
+} else {
+    console.log("Too young to watch the movie.");
+}
